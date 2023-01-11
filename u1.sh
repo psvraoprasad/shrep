8be07095decc2244bbf67dfcbff9a97c6972a022
sv <97243937+psvraoprasad@users.noreply.github.com>
2023-01-11 15:31:41 +0530

    u1.sh added

---
+++ u1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+folder_path="/Desktop/uxcmd/test1"
+zip_file_name="archive.zip"
+manifest_file_name="manifest.txt"
+
+# navigate to the folder
+cd $folder_path
+
+# zip all files in the folder and save the archive to the parent directory
+zip $zip_file_name *
+
+# navigate to the parent directory
+cd ..
+
+# create a manifest file listing the names of the zipped files
+unzip -l $folder_path/$zip_file_name > $manifest_file_name
